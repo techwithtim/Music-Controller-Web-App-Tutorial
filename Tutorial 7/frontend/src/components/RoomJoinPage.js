8f90fb4643b70c52cdd0c595410e6b715a56b06d
Tim <mrtechguy234@gmail.com>
2020-11-08 20:58:41 -0500

    added tutorial 7

---
+++ Tutorial 7/frontend/src/components/RoomJoinPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class RoomJoinPage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <p>This is the room join page</p>;
+  }
+}
